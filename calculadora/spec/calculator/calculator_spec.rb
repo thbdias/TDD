2d7d45dec92e27091462dd94c4cf8b2b9adcbdaa
thiago.dias <thiagobalbino91@gmail.com>
2018-12-19 13:20:17 -0200

    seção 2 - subject

--- calculadora/spec/calculator/calculator_spec.rb
+++ calculadora/spec/calculator/calculator_spec.rb
@@ -1,15 +1,23 @@
 require 'calculator'
 
 describe Calculator do
-  it 'use sum method for 2 numbers' do
-    calc = Calculator.new 
-    result = calc.sum(5, 7)
-    expect(result).to eq(12)
-  end
 
-  it 'use sum method for 2 numbers with negative numbers' do
-    calc = Calculator.new 
-    result = calc.sum(-5, 7)
-    expect(result).to eq(2)
-  end
+  subject (:calc) { describe_class.new }
+
+  context '#sum' do
+    it 'with positive numbers' do      
+      result = subject.sum(5, 7)
+      expect(result).to eq(12)
+    end
+
+    it 'with negative and positive numbers' do      
+      result = subject.sum(-5, 7)
+      expect(result).to eq(2)
+    end
+
+    it 'with negative numbers' do      
+      result = subject.sum(-5, -7)
+      expect(result).to eq(-12)
+    end
+  end #end context
 end
