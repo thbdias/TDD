f6e5eb8f3eef9abd3a64b5c8808e70a47499b6d5
thbdias <thiagobalbino91@gmail.com>
2018-12-26 16:28:37 -0200

    one-liner syntax(subject)

--- rspec_tests/spec/matchers/ranges/ranges_spec.rb
+++ rspec_tests/spec/matchers/ranges/ranges_spec.rb
@@ -1,7 +1,12 @@
 describe (1..5), 'Ranges' do
   it '#cover' do
-    expect(subject).to cover(2) #cubra o 2
+    is_expected.to cover(2) #is_expected == expect(subject)
     expect(subject).to cover(2, 5)  #cubra o 2 e 5
     expect(subject).not_to cover(0, 6)  #não cubra o 2 e 5
   end
+
+  it { is_expected.to cover(2) }
+  it { is_expected.to cover(2, 5) }
+  it { is_expected.not_to cover(0, 6) }
+  it { expect(subject).not_to cover(0, 6) }
 end
