ab6790c8b38e08c1e40917e15a1ac04889dee979
thbdias <thiagobalbino91@gmail.com>
2018-12-27 00:17:26 -0200

    as null object

--- rspec_tests/spec/test_doubles/user_spec.rb
+++ rspec_tests/spec/test_doubles/user_spec.rb
@@ -7,4 +7,14 @@ describe 'Test Double' do
         puts user.name
         puts user.password
     end
+   
+   
+    it '-as_null_object' do
+        user = double('User').as_null_object #qualquer chamada ao doublê, se levantar msg de erro ele vai ignorar
+        allow(user).to receive(:name).and_return('Thiago')
+        allow(user).to receive(:password).and_return('secret')
+        puts user.name
+        puts user.password
+        user.abc
+    end
 end
