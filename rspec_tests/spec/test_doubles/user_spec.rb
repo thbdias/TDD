d8358a1c4ee219f69f6eef92c0dbdd58821c740c
thbdias <thiagobalbino91@gmail.com>
2018-12-26 23:04:35 -0200

    test double - teste doublê

---
+++ rspec_tests/spec/test_doubles/user_spec.rb
@@ -0,0 +1,10 @@
+describe 'Test Double' do
+    it '--' do
+        user = double('User')
+        # allow(user).to receive_messages(name: 'Thiago', password: 'secret') #Thiago é o retorno, name: e password são messages (métodos)
+        allow(user).to receive(:name).and_return('Thiago')
+        allow(user).to receive(:password).and_return('secret')
+        puts user.name
+        puts user.password
+    end
+end
