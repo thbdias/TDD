e23cb98805bf8b37046151f68db993c5a2154ec0
thbdias <thiagobalbino91@gmail.com>
2018-12-27 00:12:58 -0200

    mock expectations

--- rspec_tests/spec/mocks/mocks_spec.rb
+++ rspec_tests/spec/mocks/mocks_spec.rb
@@ -12,4 +12,24 @@ describe 'Mocks' do
         # exercice
         student.bar 
     end
+
+
+    it 'args' do
+        student = Student.new 
+        expect(student).to receive(:foo).with(123)
+        student.foo(123)
+    end
+
+    it 'repetição' do
+        student = Student.new 
+        expect(student).to receive(:foo).with(123).twice #espera ser chamado 2x
+        student.foo(123)
+        student.foo(123)
+    end
+
+    it 'retorno' do
+        student = Student.new 
+        expect(student).to receive(:foo).with(123).and_return(123456) #forçando o retorno   
+        puts student.foo(123)
+    end
 end
