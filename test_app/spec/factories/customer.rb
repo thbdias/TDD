015e513767d070fb2788cb811424d75a4c21411f
thiago.dias <thiagobalbino91@gmail.com>
2018-12-28 12:31:39 -0200

    factory bot

---
+++ test_app/spec/factories/customer.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do 
+  factory :customer do
+    name "Thiago Dias"
+    email "thiago@dias.com"
+  end 
+end
--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   
   #fixtures :customers #invocando arquivo customers.yml
-  fixtures :all #invocando todos arquivos yml dentro de fixtures/
+  #fixtures :all #invocando todos arquivos yml dentro de fixtures/
   
   it 'Create a Customer' do    
-    customer = customers(:thiago)
+    customer = create(:customer) #usando factory bot
     expect(customer.full_name).to eq("Sr. Thiago Dias")
   end
 end
