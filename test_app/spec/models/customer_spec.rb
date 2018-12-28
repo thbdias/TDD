81265b2cae1ba394623d7fcb11f4457d90a5bf69
thiago.dias <thiagobalbino91@gmail.com>
2018-12-28 12:19:57 -0200

    fixtures

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  #fixtures :customers #invocando arquivo customers.yml
+  fixtures :all #invocando todos arquivos yml dentro de fixtures/
+  
+  it 'Create a Customer' do    
+    customer = customers(:thiago)
+    expect(customer.full_name).to eq("Sr. Thiago Dias")
+  end
 end
