1a8b52ca6a092da762214e4ba22cd12ab71d092b
thiago.dias <thiagobalbino91@gmail.com>
2018-12-28 12:44:31 -0200

    sobrescrevendo atributos e aliases

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -5,8 +5,14 @@ RSpec.describe Customer, type: :model do
   #fixtures :customers #invocando arquivo customers.yml
   #fixtures :all #invocando todos arquivos yml dentro de fixtures/
   
+  it '#full_name - Sobrescrevendo Atributo' do    
+    customer = create(:customer, name: "Thiago Dias") #usando factory bot
+    expect(customer.full_name).to eq("Sr. Thiago Dias")    
+  end
+  
   it '#full_name' do    
-    customer = create(:customer) #usando factory bot
+    # customer = create(:customer) #usando factory bot
+    customer = create(:user) #usando aliases
     expect(customer.full_name).to start_with("Sr. ")    
   end
 
