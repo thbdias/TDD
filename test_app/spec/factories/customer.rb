46e1273b92884ee2bbac3339a9f3f9614ba2c6a8
thbdias <thiagobalbino91@gmail.com>
2018-12-30 01:01:02 -0200

    atributos trasientes

--- test_app/spec/factories/customer.rb
+++ test_app/spec/factories/customer.rb
@@ -3,6 +3,10 @@ FactoryBot.define do
     name Faker::Name.name
     email Faker::Internet.email
 
+    transient do
+      upcased false
+    end
+
     factory :customer_vip do
       vip true
       days_to_pay 30
@@ -11,6 +15,10 @@ FactoryBot.define do
     factory :customer_default do
       vip false
       days_to_pay 15
+    end
+
+    after(:create) do |customer, evaluator|
+      customer.name.upcase! if evaluator.upcased
     end
 
   end 
