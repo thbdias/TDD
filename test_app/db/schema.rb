e179a3dadf6070451accdd31951ad67fab088a87
thbdias <thiagobalbino91@gmail.com>
2019-01-03 19:45:43 -0200

    traits

--- test_app/db/schema.rb
+++ test_app/db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2018_12_27_023617) do
     t.string "email"
     t.boolean "vip"
     t.integer "days_to_pay"
+    t.string "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
