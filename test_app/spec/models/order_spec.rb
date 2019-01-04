d9d9cfa75445188a724d9c0c047473dbd5b1846c
thiago.dias <thiagobalbino91@gmail.com>
2019-01-04 12:32:20 -0200

    create_list

--- test_app/spec/models/order_spec.rb
+++ test_app/spec/models/order_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 RSpec.describe Order, type: :model do
   it 'Tem 1 pedido' do
     # customer = create(:customer)
-    # order = create(:order, customer: customer)    
-    
+    # order = create(:order, customer: customer)        
     order = create(:order)    
-
     expect(order.customer).to be_kind_of(Customer)
+  end
+
+  it 'Tem 3 pedidos' do
+    #orders = create_list(:order, 3) #cria 3 instâncias
+    orders = create_list(:order, 3, description: "testesssss") #sobrescreve
+    puts orders.inspect
+    expect(orders.count).to eq(3)
   end
 end
