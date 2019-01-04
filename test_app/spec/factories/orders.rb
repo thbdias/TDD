52f0e4bd194b844505c0effe71a7e34dac1c44b5
thiago.dias <thiagobalbino91@gmail.com>
2019-01-04 12:44:38 -0200

    has_many

--- test_app/spec/factories/orders.rb
+++ test_app/spec/factories/orders.rb
@@ -3,8 +3,7 @@ FactoryBot.define do
     sequence (:description) { |n| "Pedido numero - #{n}" }
     
     #as duas linhas abaixo fazem a msm coisa
-    association :customer, factory: :customer 
-    #customer 
-
+    #association :customer, factory: :customer 
+    customer 
   end
 end
