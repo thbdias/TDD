d78fb54bf852341c83537b13cb55a8cfcba4fc68
thbdias <thiagobalbino91@gmail.com>
2019-01-05 16:44:10 -0200

    vcr - modos de gravação

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,13 +1,14 @@
 describe "HTTParty" do
     # it 'content-party', :vcr do
-    it 'content-party', vcr: {cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body]} do
+    # it 'content-party', vcr: {cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body]} do
+    it 'content-party', vcr: {cassette_name: 'jsonplaceholder/posts', :record => :new_episodes} do
         # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/1").            
         #     to_return(status: 200, body: "", headers: {
         #         'content-type': 'application/json'
         #     })
 
         #VCR.use_cassette("jsonplaceholder/posts") do
-            response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/6')
+            response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/4')
             content_type = response.headers['content-type']
             # puts content_type
             expect(content_type).to match(/application\/json/)
