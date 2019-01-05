65f3304869b24a819c3a79659344b537852df80c
thbdias <thiagobalbino91@gmail.com>
2019-01-05 16:02:44 -0200

    vcr

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,13 +1,15 @@
 describe "HTTParty" do
     it 'content-party' do
-        stub_request(:get, "https://jsonplaceholder.typicode.com/posts/1").            
-            to_return(status: 200, body: "", headers: {
-                'content-type': 'application/json'
-            })
+        # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/1").            
+        #     to_return(status: 200, body: "", headers: {
+        #         'content-type': 'application/json'
+        #     })
 
-        response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
-        content_type = response.headers['content-type']
-        # puts content_type
-        expect(content_type).to match(/application\/json/)
+        VCR.use_cassette("jsonplaceholder/posts") do
+            response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
+            content_type = response.headers['content-type']
+            # puts content_type
+            expect(content_type).to match(/application\/json/)
+        end
     end
 end
