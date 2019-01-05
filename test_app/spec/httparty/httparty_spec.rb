1fbddc366552933383ea0b6d160eb8fa6686d576
thbdias <thiagobalbino91@gmail.com>
2019-01-05 16:15:47 -0200

    vcr com metadados do rspec

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,15 +1,16 @@
 describe "HTTParty" do
-    it 'content-party' do
+    # it 'content-party', :vcr do
+    it 'content-party', vcr: {cassette_name: 'jsonplaceholder/posts'} do
         # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/1").            
         #     to_return(status: 200, body: "", headers: {
         #         'content-type': 'application/json'
         #     })
 
-        VCR.use_cassette("jsonplaceholder/posts") do
+        #VCR.use_cassette("jsonplaceholder/posts") do
             response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
             content_type = response.headers['content-type']
             # puts content_type
             expect(content_type).to match(/application\/json/)
-        end
+        #end
     end
 end
