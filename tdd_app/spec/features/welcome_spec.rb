e620120d73a60f6dbe9d4d8c4e5a91b5a52acadc
thiago.dias <thiagobalbino91@gmail.com>
2019-01-08 12:19:10 -0200

    feature spec

---
+++ tdd_app/spec/features/welcome_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature "Welcome", type: :feature do
+  
+  scenario 'Mostra a mensagem de Bem-Vindo' do
+    visit('/')
+    expect(page).to have_content('Bem-Vindo')
+  end
+
+end
