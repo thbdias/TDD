35474b6a4806a1d6d8a32be0c609dc6d92bae02e
thbdias <thiagobalbino91@gmail.com>
2019-01-08 23:21:06 -0200

    criando e testando links

--- tdd_app/spec/features/customer_spec.rb
+++ tdd_app/spec/features/customer_spec.rb
@@ -99,4 +99,33 @@ feature "Customers", type: :feature do
     expect(page).to  have_content(new_name)
   end
 
+  it 'Clica no link Mostrar da Index' do
+    customer = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ['S', 'N'].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customers_path)
+    find(:xpath, "/html/body/table/tbody/tr[1]/td[2]/a").click
+    expect(page).to have_content("Mostrando Cliente")
+  end
+
+  it 'Clica no link Editar da Index' do
+    customer = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ['S', 'N'].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customers_path)
+    find(:xpath, "/html/body/table/tbody/tr[1]/td[3]/a").click
+    expect(page).to have_content("Editando Cliente")
+  end
+
+
 end
